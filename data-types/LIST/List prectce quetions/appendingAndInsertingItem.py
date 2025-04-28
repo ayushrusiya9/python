c077feda7e5e034d776d1517802f98256016c1f4
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-04-28 10:58:26 +0530

    update

---
+++ data-types/LIST/List prectce quetions/appendingAndInsertingItem.py
@@ -0,0 +1,12 @@
+#create an empty list `fruits`.
+# - Use `append()` to add `"apple"`, `"banana"`, and `"cherry"` one by one.
+# - Use `insert()` to add `"mango"` at position 1.
+# - Print the final list.
+
+fruits = []
+fruits.append("Apple")
+fruits.append("Banana")
+fruits.append("Cherry")
+fruits.insert(1, "Mango")
+
+print(fruits)
