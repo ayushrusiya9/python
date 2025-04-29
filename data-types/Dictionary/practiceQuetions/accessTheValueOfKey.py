3a3b33ad9679d4a0d4da9a485902a3c16d9c383e
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-04-29 18:25:55 +0530

    update

--- data-types/Dictionary/practiceQuetions/accessTheValueOfKey.py
+++ data-types/Dictionary/practiceQuetions/accessTheValueOfKey.py
@@ -4,3 +4,16 @@
 student = {'name': "Aashu", 'age': 22, 'city': 'Bhopal'}
 
 print(student['city'])
+
+#Update the value of 'age' to 23.
+s = {'age': 23}
+student.update(s)
+print(student)
+
+# Add a new key 'course' with value 'B.Tech'.
+student.setdefault('course','B.tech')
+print(student)
+
+# Delete the key 'city' safely.
+student.pop('city')
+print(student)
