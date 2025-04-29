c4ffac23b9cc4925a50879580314cf0486c55097
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-04-29 18:17:31 +0530

    update

---
+++ data-types/Dictionary/practiceQuetions/accessTheValueOfKey.py
@@ -0,0 +1,6 @@
+#Access the value of key 'city' from a dictionary:
+#    student = {'name': 'Aashu', 'age': 22, 'city': 'Bhopal'}
+
+student = {'name': "Aashu", 'age': 22, 'city': 'Bhopal'}
+
+print(student['city'])
---
+++ data-types/Dictionary/practiceQuetions/createDictionaryFromTwoList.py
@@ -0,0 +1,15 @@
+# # Create a dictionary from two lists:
+#    keys = ['name', 'age', 'city']
+#    values = ['Aashu', 22, 'Bhopal']
+#    # Make a dict out of this
+
+keys = ['name', 'age', 'city']
+valuess = ['Ashu', 22, 'Bhopal']
+
+d = {
+    keys[0]: valuess[0],
+    keys[1]: valuess[1],
+    keys[2]: valuess[2],
+}
+
+print(d)
