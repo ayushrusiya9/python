dd1f36de58f3e3f2fcb659331c7dbf0d0e7f4509
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-04-29 09:44:16 +0530

    update

---
+++ data-types/LIST/tuplePracticeQuetions/convertingBetweenDataStructure.py
@@ -0,0 +1,14 @@
+#Convert the list [10, 20, 30] into a tuple, and a string "hello" into a tuple of characters. 
+
+l = [10,20,30]
+t = tuple(l)
+print(t)
+print(type(t))
+
+s = "hello"
+ts = tuple(s)
+print(ts)
+print(s)
+print(type(s))
+print(ts)
+print(type(ts))
---
+++ data-types/LIST/tuplePracticeQuetions/tupleSortingChallenge.py
@@ -0,0 +1,2 @@
+#Sort the list of tuples [(1, 3), (2, 2), (3, 1)] based on the second element of each 
+
