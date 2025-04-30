e3d72052f928d50cdc9e86c2dc20407a7504e1c2
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-04-30 15:11:08 +0530

    update

---
+++ data-types/Set/set.py
@@ -0,0 +1,21 @@
+# unordered collection of unique element -(homoginus, hitrog)
+#indexing not suppor and also slicing are not support
+
+s = {10, 20, 30, 10, 40, 'python', 'java','php'}
+
+print(s)
+print(type(s))
+print(id(s))
+print(len(s))
+# print(max(s))
+# print(min(s))
+# print(sum(s))
+
+s1 = {'python','java','php'}
+print(min(s1))
+print(max(s1))
+
+
+s2 = {10,20,20,20}
+print(sum(s2))
+
---
+++ data-types/Set/setMethods.py
@@ -0,0 +1,8 @@
+#add,update,remove,discard ye importnt hai
+s = {10,23,32,'python','php'}
+s.add('juj')
+print(s)
+s.update('php','go')
+# s.update(10,20)
+s.update([12,32])
+print(s)
