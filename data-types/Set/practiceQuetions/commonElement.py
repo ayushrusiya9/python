2f0843b1df4bf71b58303e58a80b70691300b4d0
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-05-05 09:55:03 +0530

    update

---
+++ data-types/Set/practiceQuetions/commonElement.py
@@ -0,0 +1,57 @@
+# # # Find common elements between two lists.
+
+# # a = [1,2,3]
+# # b = [3,4,5]
+
+# # au = set(a)
+# # bu = set(b)
+
+# # c = au.intersection(bu)
+# # c = list(c)
+
+# # print(c)
+
+# a = [1,2,3,4]
+# b = [3,4,5,6]
+
+# au = set(a)
+# bu = set(b)
+
+# c = au.union(bu)
+# c = list(c)
+# print(c)
+
+# import keyword
+
+# l = keyword.softkwlist
+# print(l)
+
+# import string
+
+# l = string.punctuation
+# print(l)
+
+# a = 5
+# b = 10
+
+# a = a * b
+# b = a / b
+# a = a / b
+# print(a)
+# print(b)
+
+# n = eval(input("enter: "))
+# # print(type(n))
+# # print(id(n))
+# print(n**(1/3))
+
+
+# print(3.14*n**2)
+
+a = (1,2,30)
+b = (1,2,3)
+print(a is b)
+
+a = 1
+b =1
+print(a is b)
