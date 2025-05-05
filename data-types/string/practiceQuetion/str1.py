15101ae46f3b7185bcc4216410cf1367e516b1a8
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-05-05 19:39:02 +0530

    update

---
+++ data-types/string/practiceQuetion/str1.py
@@ -0,0 +1,65 @@
+# Take a string and print its length.
+
+# s = "ayush rusiya"
+# print(len(s))
+
+# Check if a string contains only alphabets.
+
+# print(s.isalpha())#they give tru
+
+# Count vowels and consonants in a string.
+# print(s.count('A'))
+
+# Reverse a string without using [::-1].
+
+# print("".join(reversed(s)))
+
+# Swap the case of each character in a string.
+
+# print(s.swapcase())
+
+#Extract the last 3 characters from a string.
+
+# print(s[9:12:1])
+
+# Print every second character from a string.
+
+# print(s[0:12:2])
+
+# Remove the first and last character from a string.
+# print(s[1:-1])
+
+# Slice and return a string in reverse word order.
+# new = s[1:-1]
+# print("".join(reversed(new)))
+# Extract all characters at even indexes.
+
+# print(s[2:-1:2])
+
+# Convert a string into title case without using .title().
+
+# print(s.capitalize())
+# Replace all spaces with hyphens.
+
+s = "ayush-rusiya"
+# print(s.replace("-",","))
+# Remove all non-alphanumeric characters.
+
+# Count the number of times a word appears in a sentence.
+# print(s.count("u"))
+# Find the first and last position of a character in a string.
+print(s[0])
+# print(s[-1])
+
+# Convert a sentence into a dictionary with word frequencies.
+
+# s1 = dict(s.count())
+# print(s1)
+# # Group words in a list that are anagrams.
+# s1 = list(s)
+# print(s1)
+# Count duplicate words in a paragraph.
+
+# Extract hashtags from a social media caption.
+
+# Extract all email addresses from a long string.
