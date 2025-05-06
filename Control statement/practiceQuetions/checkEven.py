b76dcd67717c550dd09269790c56fda2629111a6
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-05-06 19:32:56 +0530

    update

---
+++ Control statement/practiceQuetions/checkEven.py
@@ -0,0 +1,6 @@
+num = int(input("Enter number : "))
+
+if num % 2 == 0 :
+    print(f"Number {num} is even.")
+else :
+    print(f"NUmber {num} is odd.")        
---
+++ Control statement/practiceQuetions/largestAmountTwoNumbers.py
@@ -0,0 +1,6 @@
+num1, num2 = int(input("Enter first number : ")), int(input("Enter second number : "))
+
+if num1 > num2 :
+    print(f"{num1} is greater then {num2}")
+else :
+    print(f"{num2} is greater then {num1}")    
