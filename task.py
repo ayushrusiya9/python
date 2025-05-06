d187bcdfe46aad0d6fdd625e10602c7eb972cef8
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-05-06 10:51:12 +0530

    updat

--- task.py
+++ task.py
@@ -4,13 +4,32 @@
 # x = tuple(x)
 # x = dict(x) # ye type cast support nahi kerta
 # x = str(x)
-x = {1:"ayush", 2 : "bhai"}
-d = frozenset(x)
-d = set(x)
-d = tuple(x)
-d = list(x)
-d = str(x) # ye important hai
-# print(d)
+# x = {1:"ayush", 2 : "bhai"}
+# d = frozenset(x)
+# d = set(x)
+# d = tuple(x)
+# d = list(x)
+# d = str(x) # ye important hai
+# # print(d)
 
 # s = "PythonIsFun"
 # print(s[::3] + s[::-4])
+
+# print(11 / 2)
+
+a = 19
+b = 21
+print("a ",a)
+print("b ",b)
+
+# a = a * b
+# b = a // b
+# a = a // b
+
+
+a = a + b
+b = a - b
+a = a - b
+
+print("a ",a)
+print("b ",b)
