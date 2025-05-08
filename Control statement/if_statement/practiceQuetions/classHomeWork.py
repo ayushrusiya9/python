353bc057ca90880719bf0dd1be4dc00896d23b28
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-05-08 10:38:33 +0530

    update

---
+++ Control statement/if_statement/practiceQuetions/classHomeWork.py
@@ -0,0 +1,31 @@
+# run time ma usr se value lena hai or check krna hai ki konsa data type hai if eslse ke through check krna hai  
+
+val = eval(input("Enter value : "))
+# print(type(val))
+# integer = int()
+# decimal = float()
+# com = complex()
+# s = str()
+# l = list()
+# t = tuple()
+# sets = set()
+# d = dict()
+
+if type(val) == type(int()) :
+    print(f"Given value {val} is integer.")
+elif type(val) == type(float()) :
+    print(f"Given value {val} is float.")
+elif type(val) == type(complex()):
+    print(f"Given value {val} is complex.")
+elif type(val) == type(str()) :
+    print(f"Given value {val} is string.")
+elif type(val) == type(list()) :
+    print(f"Given value {val} is list.")
+elif type(val) == type(tuple()):
+    print(f"Given value {val} is tuple.")
+elif type(val) == type(set()) :
+    print(f"Given value {val} is set.")
+elif type(val) == type(dict()) :
+    print(f"Given value {val} is dictionary.")
+else :
+    print(f"Given program is not run!")
