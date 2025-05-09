0bedf7effa2e97e8f9fb2131712baf6c310b90fd
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-05-09 10:51:47 +0530

    update

---
+++ Control statement/itrative/whileloop/practiceQuetions/sum_of_even_number.py
@@ -0,0 +1,19 @@
+#sum of even number
+
+n = int(input("Enter number: "))
+
+i = 1
+sum = 0
+while i <= n:
+
+    if i % 2 == 0 :
+        sum = sum + i
+
+        if i < (n - 1):
+             print(i, end=" + ")
+        else :
+            print(i, end=" = ")     
+
+    i = i + 1
+
+print(sum)      
