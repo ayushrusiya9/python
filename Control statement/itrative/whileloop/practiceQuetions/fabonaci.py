5c55107557e81d6fb72005740884e9d1ae1d6265
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-05-10 15:20:06 +0530

    update

---
+++ Control statement/itrative/whileloop/practiceQuetions/fabonaci.py
@@ -0,0 +1,13 @@
+n = int(input("Enter number: "))
+
+x,y,i = 0, 1, 1
+
+while i <= n :
+
+    z = x + y
+    x, y = y, z
+    if i < n :
+        print(z, end=",")
+    else:
+        print(z)    
+    i = i + 1   
