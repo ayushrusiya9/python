b6d51655553d47447b5af7800bcbcc413a66bcfe
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-05-22 15:40:31 +0530

    update

---
+++ OOP's/class1.py
@@ -0,0 +1,14 @@
+class Student:
+
+    def __init__(self,name, city, contect):
+        self.n = name
+        self.c = city
+        self.co = contect
+
+# obj = Student
+# obj = Student()
+obj = Student('Ayush','bhopal',231)
+print(obj.n,obj.c,obj.co)
+
+obj1 = Student("Aditya",'sehore',23123243)
+print(obj1.n, obj1.c,obj1.co)
---
+++ OOP's/variable.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self,name,city):
+        self.n = name #inside constructer // instance veriable
+        self.c = city
+        print(self.n,self.c)#access inside constructer    
+
+    def add(self,phone):
+        self.p = phone #inside instance method (decleration)
+        print(self.n,self.c,self.school,self.p)#access inside constructer
+
+
+
+obj = Student('ayush','Bhopal') 
+# obj.add(1232)
+# print(obj.n,obj.c,obj.school,obj.p)
+obj.school = 'shhs' #out-side of the class(decleration)
+# print(obj.n,obj.c,obj.school,obj.p)
+obj.add(1232)
+print(obj.n,obj.c,obj.school,obj.p)
