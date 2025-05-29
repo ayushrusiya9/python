544e774d4833c1f721fe05f262f39087eaa3de49
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-05-29 15:26:05 +0530

    update

--- convensional_methods.py
+++ convensional_methods.py
@@ -1,6 +1,15 @@
 l = [10,2,4,6,8,20]
 
+for i in range(len(l)-1):
+    if l[i] > l[i + 1]:
+        l[i],l[i + 1] = l[i + 1],l[i]
+# l.sort()
+print(l)
+# l.reverse()
 
-for i in range(0,len(l)):
-    if i < i + 1:
-        
+for i in range(len(l)):
+    for j in range(len(l) - i - 1):
+        if l[i] > l[i + 1]:
+            l[i],l[i + 1] = l[i + 1],l[i]
+
+print(l)
