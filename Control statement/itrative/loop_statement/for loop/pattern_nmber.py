b3b2bbdd161588b3f29623a77a9fe413a47a6164
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-06-02 11:17:30 +0530

    update

---
+++ Control statement/itrative/loop_statement/for loop/pattern_nmber.py
@@ -0,0 +1,11 @@
+n = int(input("Enter number: "))
+# n = 5
+
+# for i in range(1,n + 1):
+#     for j in range(1, i + 1):
+#         print(" "*(n - i),i ,end="")
+#     print()
+
+for i in range(1,n+1):
+    print(' ' * (n - i), end="")
+    print((str(i) + ' ') * i)
