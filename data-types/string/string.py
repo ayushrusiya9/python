2877cd43ce35a8ab7e352d7edcc55ec0bac6bdc0
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-08-28 10:39:02 +0530

    update

--- data-types/string/string.py
+++ data-types/string/string.py
@@ -3,16 +3,21 @@
 
 #+,* inhi per lgta hai
 
-s1 = "ayus"
-s2 = "rus"
-s3 = s1+s2
-print(s3, type(s3))#type variable per depand hoga
+# s1 = "ayus"
+# s2 = "rus"
+# s3 = s1+s2
+# print(s3, type(s3))#type variable per depand hoga
 
-s1 = "ayus"
-s2 = "rus"
-s3 = s1+s2
-print(s3*s2)
+# s1 = "ayus"
+# s2 = "rus"
+# s3 = s1+s2
+# print(s3*s2)
 
-s1 = "ayus"
-s2 = 2
-print(s1 * s2)
+# s1 = "ayus"
+# s2 = 2
+# print(s1 * s2)
+
+
+# ayu =  'aYush'
+a = (1,)
+print(len(a))
