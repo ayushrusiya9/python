9739b7361f64cd3ac5ed54f31f8f91aebc03edd9
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-09-25 19:37:39 +0530

    update

--- function/higher order function/map().py
+++ function/higher order function/map().py
@@ -2,14 +2,15 @@
 def add5(n):
     return n + 5
 
-def add5(n,m):
-    # return n + m
-    return n ** m
+# def add5(n,m):
+#     # return n + m
+#     return n ** m
 
 l = [1,2,3,4,5]
 l1 = [1,2,3,4,5]
 
-x = map(add5,l,l1)
+# x = map(add5,l,l1)
+x = map(add5,l)
 print(x)
 print(tuple(x))
-# print(list(x))#yha per isliye nahi aya kyuki pehle ye sab data de chula tha
+# print(list(x))#yha per isliye nahi aya kyuki pehle ye sab data de chuka tha
---
+++ function/higher order function/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+
+#     # return n + m
+#     return n ** m
