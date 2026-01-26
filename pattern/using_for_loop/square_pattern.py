7be34c2f8913d407a5e84a9a1d4e9e2a1af32122
Ayush Rusiya <ayushrusiya9@gmail.com>
2026-01-26 23:10:12 +0530

    update

--- pattern/using_for_loop/square_pattern.py
+++ pattern/using_for_loop/square_pattern.py
@@ -2,3 +2,11 @@ row = int(input("ENTER NUMBER: "))
 
 for i in range(1, row + 1):
     print(" * " * row)
+
+def square_pattern(n):
+    for i in range(n):
+        for j in range(n):
+            print("*", end=" ")
+        print() 
+        
+square_pattern(row)
