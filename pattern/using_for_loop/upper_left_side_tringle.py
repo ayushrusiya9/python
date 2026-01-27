167a9977bcf29335394972a1c8f4ffa9936d1b6d
Ayush Rusiya <ayushrusiya9@gmail.com>
2026-01-27 22:18:43 +0530

    update

--- pattern/using_for_loop/upper_left_side_tringle.py
+++ pattern/using_for_loop/upper_left_side_tringle.py
@@ -2,3 +2,10 @@ row = int(input("ENTER NUMBER: "))
 
 for i in range(0, row):
     print('*'*(row-i))
+
+def upper_left_side_triangle(n):
+    for i in range(n):
+        for j in range(n - i):
+            print("*", end=" ")
+        print() 
+upper_left_side_triangle(row)
