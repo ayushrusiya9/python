389a47acc8b1fa609e1e3e379690f5fb3d4eea7f
Ayush Rusiya <ayushrusiya9@gmail.com>
2026-01-27 22:19:29 +0530

    update

--- pattern/pyramid.py
+++ pattern/pyramid.py
@@ -2,3 +2,13 @@ row = int(input("ENTER NUMBER: "))
 
 for i in range(1, row + 1):
     print(' '*(row - i) + ' *'* i )
+
+def pyramid_pattern(n):
+    for i in range(n):
+        for j in range(n - i - 1):
+            print(" ", end="")
+        for k in range(i + 1):
+            print("* ", end="")
+        print() 
+
+pyramid_pattern(row)
